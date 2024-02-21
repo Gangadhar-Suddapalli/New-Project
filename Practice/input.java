508b6ca996a7ae2508c98a228e246f753d6571f6
ganga <ganga@192.168.0.204>
2024-02-21 11:17:08 +0530

    Commit

---
+++ Practice/input.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+import java.util.*;
+public class input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url="jdbc:mysql://localhost:3306/Gangadhar";
+		String userName="root";
+		String  password="Gangadhar@1";
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet res = null;
+
+	}
+
+}
